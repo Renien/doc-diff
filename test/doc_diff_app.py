867cffdbaeebbfbf29cdc0a1cb1d174f746452ef
Renien <renien.john@gmail.com>
2016-07-01 09:57:13 +0530

    sample test app

---
+++ test/doc_diff_app.py
@@ -0,0 +1,17 @@
+from doc_diff import Diff
+from doc_diff import gen_comp_report
+
+if __name__ == '__main__':
+    # Data file location
+    a_priori_csv_location = "../data/a-priori.csv"
+    pfp_csv_location = "../data/pfp.csv"
+
+    # Process a-priori.csv data file
+    a_priori_diff = Diff(a_priori_csv_location)
+    a_priori_diff.process_file()
+
+    # Process pfp.csv data file
+    pfp_diff = Diff(pfp_csv_location)
+    pfp_diff.process_file()
+
+    gen_comp_report(a_priori_diff, pfp_diff)
