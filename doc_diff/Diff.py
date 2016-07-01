6b6c90237fec3e5e0a98536f7b6ba13d440e5fc6
Renien <renien.john@gmail.com>
2016-07-01 09:56:43 +0530

    added more features to diff i.e process the file and to get the diff of two files

--- doc_diff/Diff.py
+++ doc_diff/Diff.py
@@ -1,9 +1,18 @@
 import csv
 import os.path as path
+import datetime
 
 
 def gen_comp_report(diff1, diff2):
-    _compare_two_files(diff1.data_dict, diff2.data_dict)
+    evaluation_results = _compare_two_files(diff1.data_dict, diff2.data_dict)
+
+    now = datetime.datetime.now()
+
+    # write the results for corresponding files
+    _write_out_results(evaluation_results[0], '../results/matching_list_' + now.strftime("%Y-%m-%d") + '.csv')
+    _write_out_results(evaluation_results[1], '../results/un_matching_list_' + now.strftime("%Y-%m-%d") + '.csv')
+    _write_out_results(evaluation_results[2], '../results/unknown_list_' + now.strftime("%Y-%m-%d") + '.csv')
+    _write_out_results(evaluation_results[3], '../results/additional_list_' + now.strftime("%Y-%m-%d") + '.csv')
 
 
 def _write_out_results(data, filename):
@@ -44,28 +53,30 @@ def _compare_two_files(dict1, dict2):
             unknown_list[key] = value
 
     additional_list = {k: v for k, v in dict2.iteritems() if k not in temp_known_keys}
-    return additional_list
+    return matching_list, un_matching_list, unknown_list, additional_list
 
 
 class Diff:
-    def __init__(self):
+    def __init__(self, file):
         self.fileT_type = 'csv'
         self.data = None
-        self._file_name = None
+        self._file_name = file
         self._res = path.join(path.dirname(__file__), 'res')
         self._no_of_records = 0
         self._delimiter = '\t'
         self.data_dict = dict()
 
     def read_file(self):
-        with open(self.file_name, 'r') as ins:
+        with open(self._file_name, 'r') as ins:
             for line in ins:
-                print line
+                self.increment_recommendation_count()
+                self.construct_key_value(line, self._delimiter)
 
     def increment_recommendation_count(self):
         self._no_of_records += 1
 
-    def format_order_recommendations(self, data):
+    @staticmethod
+    def format_order_recommendations(data):
         tem_list = data.replace('\n', '').replace('\"\"', '').split(',')
         tem_list.sort()
         return tem_list
@@ -76,3 +87,6 @@ class Diff:
             self.data_dict[key] = ','.join(self.format_order_recommendations(value))
         else:
             print "The key already exist : %s" % key
+
+    def process_file(self):
+        self.read_file()
