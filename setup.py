ddb227b6661fa0f3f94bccb3305c4824840d84bc
Renien <renien.john@gmail.com>
2016-07-02 17:06:36 +0530

    added the python packaging 'setup.py'

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='doc-diff',
+    version='1.0.0',
+    description='support lib to generate the diff of two CSV files',
+    url='https://github.com/Renien/doc-diff',
+    author='Renien',
+    author_email='renien.john@gmail.com',
+    license='MIT',
+    packages=find_packages(exclude=['test'])
+)
